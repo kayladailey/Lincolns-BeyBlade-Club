f9b53f754628808eb1ef5fe744851b572ecd4cb5
Kayla Dailey <kayladailey000@gmail.com>
2019-11-26 23:24:22 -0600

    created 2 more counter components

--- reduxx-app/src/App.js
+++ reduxx-app/src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-
 import Lincoln from './components/Lincoln';
 import Kenzie from './components/Kenzie';
-import { Card } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
 
     function App() {
-
       return (
         <div className="App">
-        <Card.Container>
+          <div className = 'card-container'>
           <Kenzie/>
           <Lincoln />
-        </Card.Container>
-
+        </div>
         </div>
           );
         }
